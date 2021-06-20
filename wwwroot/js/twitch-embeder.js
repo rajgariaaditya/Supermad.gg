9133a24f4bb1fdcab731bb55024d53dfd8f94d51
Aditya Rajgaria <aditya.rajgaria@hotmail.com>
2021-06-19 23:26:33 -0400

    initial design

---
+++ wwwroot/js/twitch-embeder.js
@@ -0,0 +1,10 @@
+﻿export function embedTwitch(id) {
+	var host = window.location.hostname;	
+	new Twitch.Embed(id, {
+		layout: "video",
+		width: "100%",
+		height: 500,
+		channel: "supermad",
+		parent: host
+	});
+}
